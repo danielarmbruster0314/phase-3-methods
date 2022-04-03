fa88bde87eeb4509bfca22a730e13b9a41e8d253
Daniel Armbruster <danielgundam00@gmail.com>
2022-04-03 13:53:16 -0600

    commit tree 7ed2cf36f471aaaee5433fd81fec120800234aa0

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+def my_method param
+    puts "Runing my_method"
+    param + 1
+end
+
+
+def greet_programmer 
+  puts "Hello, programmer!"
+end
+
+
+def greet name  
+  puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = "programmer") 
+  puts "Hello, #{name}!"
+end
+
+
+
+def add num1, num2 
+  return num1 + num2
+end
+
+
+def halve number  
+    return nil unless number.class == Integer
+
+  number / 2
+end
+
+
